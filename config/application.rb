3ea33dd353155f80b0a9686c0fb38ade87b2ec5e
tly <at.dyn-20iopl0@outlook.jp>
2021-10-17 11:51:05 +0000

    create database

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # ドメイン「amazon.com」の許可
+    config.hosts << ".amazonaws.com"
   end
 end
