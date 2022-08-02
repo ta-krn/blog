ecefe920b583c7bac9ede8dc2e4bb0c53617d088
tly <at.dyn-20iopl0@outlook.jp>
2022-08-02 12:15:14 +0000

    Add user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :like_articles]
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy, :followings, :followers, :like_articles]
+  before_action :set_user, only: [:show, :destroy, :followings, :followers, :like_articles]
   
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 3)
   end
   
   def show
-    @user = User.find(params[:id])
     @pagy, @articles = pagy(@user.articles.order(created_at: :desc), items:5)
     counts(@user)
   end
@@ -23,24 +23,42 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new
+      render :show
     end
   end
   
+  def edit
+    @user = User.find(current_user.id)
+  end
+  
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:success] = '更新されました'
+      redirect_to @user
+    else
+      flash.now[:danger] = '更新されませんでした'
+      render :show
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = '削除されました'
+    redirect_to :root
+  end
+  
   def followings
-    @user = User.find(params[:id])
     @pagy, @followings = pagy(@user.followings)
     counts(@user)
   end
 
   def followers
-    @user = User.find(params[:id])
     @pagy, @followers = pagy(@user.followers)
     counts(@user)
   end
 
   def like_articles
-    @user = User.find(params[:id])
     @pagy, @like_articles = pagy(@user.like_articles.order(created_at: :asc), items:5)
     counts(@user)
   end
@@ -49,5 +67,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
