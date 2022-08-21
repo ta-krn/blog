0f198c0ee0a07f77f655df4dc0eb0e6de89819b6
tly <at.dyn-20iopl0@outlook.jp>
2022-08-21 04:37:54 +0000

    remove unnecessary code from likes_controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,10 @@ class LikesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     current_user.like(@article)
-    # flash[:success] = 'いいねしました。'
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     current_user.unlike(@article)
-    # flash[:success] = 'いいねを解除しました。'
-    # redirect_back(fallback_location: root_path)
   end
 end
