a8134698c30f46cb255a168df94ff31ec0150e1b
tly <at.dyn-20iopl0@outlook.jp>
2022-08-27 17:56:45 +0000

    tab in user page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,29 @@ ActiveStorage.start()
 //= require jquery
 //= require rails-ujs
 require('./preview')
+
+// タブ機能
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    // タブに対してクリックイベントを適用
+    const tabs = document.getElementsByClassName('tab');
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener('click', tabSwitch, false);
+    }
+
+    // タブ切り替え
+    function tabSwitch() {
+      document
+        .getElementsByClassName('is-active')[0]
+        .classList.remove('is-active');
+      this.classList.add('is-active');
+
+      document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+      const arrayTabs = Array.prototype.slice.call(tabs);
+      const index = arrayTabs.indexOf(this);
+      document.getElementsByClassName('panel')[index].classList.add('is-show');
+    }
+  },
+  false
+);
